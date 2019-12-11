7fdf7aa909654ed5ff992425d98f0d45dce07f35
Will Wolman <Will@WFD311-10m.local>
2019-12-11 17:13:01 -0500

    Commit Before Branch

--- js/index.js
+++ js/index.js
@@ -1,1 +1,3 @@
-document.getElementById('btnOthers') .addEventListener('click')
+document.getElementById('btnOthers').addEventListener('click', event => {
+    document.getElementById('others').classList.toggle('open')
+  })
